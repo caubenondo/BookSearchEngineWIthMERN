53e49b9b2d65270a4382733bcbbdceab2d4fa122
Hai Duong <caubenondo@gmail.com>
2022-09-07 00:08:05 -0700

    deplying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// addstuff
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernbook', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
