1dc33ff7617e8d4691dea56382ee4a5549e241d7
Hai Duong <caubenondo@gmail.com>
2022-09-07 00:24:30 -0700

    access everywhere

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// change mongodb atlas
+// change mongodb atlas access everywhere
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernbook', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
